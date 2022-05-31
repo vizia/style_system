220aca65c37538391bf743cc0d70b21444138bce
kdecay <kdecaymusic@protonmail.com>
2022-05-31 17:43:56 +0200

    Small changes and tests

--- src/values/alpha.rs
+++ src/values/alpha.rs
@@ -5,3 +5,19 @@ pub struct AlphaValue(pub f32);
 
 impl_parse_try_parse!(AlphaValue => Percentage, f32);
 impl_from_newtype!(AlphaValue => f32, Percentage);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::assert_parse_value;
+
+    #[test]
+    fn parse_alpha_number() {
+        assert_parse_value!(AlphaValue, "0.3", AlphaValue(0.3));
+    }
+
+    #[test]
+    fn parse_alpha_percentage() {
+        assert_parse_value!(AlphaValue, "30%", AlphaValue(0.3));
+    }
+}
